a7211cab799ef3557c5a0ee92cc14b33fce78edd
lazycoder1 <gautamgsabhahit@gmail.com>
2022-03-31 01:27:07 +0530

    fixing typo

--- src/components/ManagedNFTs.vue
+++ src/components/ManagedNFTs.vue
@@ -19,11 +19,11 @@
                 <v-card class="secondary">
                   <v-img :src="nft.tokenURI" height="250" width="300" />
                   <v-card-title class="plain--text">NAME</v-card-title>
-                  <v-card-subtitle class="plain--text">Token Id: {{nft.tokenId}}</v-card-subtitle>
+                  <v-card-subtitle class="plain--text">Token Id: {{ nft.tokenId }}</v-card-subtitle>
                 </v-card>
               </v-row>
               <v-row justify="center">
-                <v-btn color="accent" dark v-bind="attrs" v-on:click="open_dialog(nft.tokenId)" > Options </v-btn>
+                <v-btn color="accent" dark v-bind="attrs" v-on:click="open_dialog(nft.tokenId)"> Options </v-btn>
               </v-row>
             </div>
           </template>
@@ -36,7 +36,9 @@
             <v-divider></v-divider>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn text @click="submit('transfer', nft.tokenId)"> Transfer </v-btn>
+
+              <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
+              <v-btn color="primary" text @click="submit('transfer', nft.tokenId)"> Transfer </v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -55,11 +57,10 @@ export default {
     address: "",
   }),
   computed: {
-    getNFTs(){
-      
-      if (this.$store.state.dataList_ManagedNFTs.nfts == null || this.$store.state.dataList_ManagedNFTs.nfts == {}) return null;
+    getNFTs() {
+      if (this.$store.state.dataList_ManagedNFTs.nfts == null || this.$store.state.dataList_ManagedNFTs.nfts == {})
+        return null;
       return this.$store.state.dataList_ManagedNFTs.nfts;
-     
     },
     getConnectedAccount() {
       console.log(this.$store.state.walletModule.account);
